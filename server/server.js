1e010daa23b2542dc06537334aa8b4f03386f0d3
Brian Haller <brianshaller@gmail.com>
2017-12-25 16:54:12 -0700

    another commit

--- server/server.js
+++ server/server.js
@@ -69,7 +69,7 @@ app.put("/editrecipes/:id", function(req, res) {
   );
 });
 
-app.use(express.static("../build"));
+app.use(express.static("build"));
 app.get("*", (req, res) =>
   res.sendFile(path.join(__dirname, "../build/index.html"))
 );
